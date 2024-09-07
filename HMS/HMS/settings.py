168d95df7d62bef56a9b3ee45eac72f5c6e46b18
unknown <princeuruakpa@gmail.com>
2024-09-07 14:41:24 +0100

    Allowed host

--- HMS/HMS/settings.py
+++ HMS/HMS/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'on9*bf65je3#4+jphqufropk!s9*i&$*54@_9t8^6+c)iro2&q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['x-empire.onrender.com']
+ALLOWED_HOSTS = ['x-empire.onrender.com', 'local host', '127.0.0.1']
 
 
 # Application definition
