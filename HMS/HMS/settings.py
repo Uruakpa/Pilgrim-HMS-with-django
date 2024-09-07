94ff512b11f98759681f0eddd74a153e9b2f2f8c
unknown <princeuruakpa@gmail.com>
2024-09-07 12:53:02 +0100

    Allowed host added

--- HMS/HMS/settings.py
+++ HMS/HMS/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'on9*bf65je3#4+jphqufropk!s9*i&$*54@_9t8^6+c)iro2&q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['x-empire.onrender.com']
 
